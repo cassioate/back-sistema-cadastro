07e6f22004a47f942e29786e9785cebd0e070cf1
Cassio <cassioate@gmail.com>
2021-06-15 18:32:09 -0300

    problema de cors

--- src/main/java/br/com/tessaro/model/Aluno.java
+++ src/main/java/br/com/tessaro/model/Aluno.java
@@ -19,6 +19,6 @@ public class Aluno {
 	private Long id;
 	private String nome;
 	private String materia;
-	private Integer nota;
+	private Double nota;
 	
 }
--- src/main/java/com/tessaro/config/CorsConfiguration.java
+++ src/main/java/com/tessaro/config/CorsConfiguration.java
@@ -1,7 +1,6 @@
 
 package com.tessaro.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,13 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan
 @Configuration
-public class CorsConfigurations implements WebMvcConfigurer {
+public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    @Bean
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            .allowedOrigins("*")
+            .allowedMethods("*");
     }
-    
 }
